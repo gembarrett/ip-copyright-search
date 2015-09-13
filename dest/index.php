da4acb4d277b132c0358fbdd436b37caa6ec4eb1
Gem Barrett <gem@gembarrett.com>
2015-09-13 03:49:05 -0400

    separating worldcat key into variable

--- dest/index.php
+++ dest/index.php
@@ -2,10 +2,13 @@
 
 // Initialise curl resource
 $curl = curl_init();
-// Set some options - we are passing in a useragent too here
+
+$worldcatKey = "l45E9GwC2Yex8qKqXdxkNznjn1mVGdG2ZjGLcaGxl9JBGX0AacC2aWmrGLXQkqF0nYvnk0BmVnSOMwUs";
+
+// set options
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=APIs&wskey=l45E9GwC2Yex8qKqXdxkNznjn1mVGdG2ZjGLcaGxl9JBGX0AacC2aWmrGLXQkqF0nYvnk0BmVnSOMwUs'
+    CURLOPT_URL => 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=APIs&wskey='.$worldcatKey
 ));
 // Send curl request, save response
 $resp = curl_exec($curl);
