042e1fc9ce76eb608e241b4f75b3744afdc29d9d
Gem Barrett <gem@gembarrett.com>
2015-09-13 03:54:47 -0400

    breaking down the worldcat requests even further

--- dest/index.php
+++ dest/index.php
@@ -3,12 +3,14 @@
 // Initialise curl resource
 $curl = curl_init();
 
-$worldcatKey = "l45E9GwC2Yex8qKqXdxkNznjn1mVGdG2ZjGLcaGxl9JBGX0AacC2aWmrGLXQkqF0nYvnk0BmVnSOMwUs";
+$worldcatKey = 'l45E9GwC2Yex8qKqXdxkNznjn1mVGdG2ZjGLcaGxl9JBGX0AacC2aWmrGLXQkqF0nYvnk0BmVnSOMwUs';
+$worldcatCatalogRequest = 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=APIs&wskey='.$worldcatKey;
+$worldcatCitationRequest = 'http://www.worldcat.org/webservices/catalog/content/citations/'.$worldcatCatalogNo.'?wskey='.$worldcatKey;
 
 // set options
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=APIs&wskey='.$worldcatKey
+    CURLOPT_URL => $worldcatCatalogRequest
 ));
 // Send curl request, save response
 $resp = curl_exec($curl);
