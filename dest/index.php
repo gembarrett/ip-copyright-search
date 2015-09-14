859852d243e4eb7b7e08c899d0b599b0b4a030a7
Gem Barrett <gem@gembarrett.com>
2015-09-14 05:22:22 -0400

    removed need for search.php (remove later) by posting to same page and keeping boxes checked

--- dest/index.php
+++ dest/index.php
@@ -5,19 +5,28 @@
   <script src="main.min.js"></script>
 </head>
 <body>
-  <form action='search.php' method='GET'>
+  <form method='GET'>
         <input type='text' name='search-keywords' size='30'
                 value='<?php echo $_GET['search-keywords']; ?>' /> 
         <input type='submit' value='search' />
         
         <div class="criteria">
-          <input type="checkbox" name="safeCreative" />
+          <input type="checkbox" name="safeCreative" <?php if(isset($_GET['safeCreative'])) echo "checked='checked'"; ?> />
           <label>Search SafeCreative</label>
         </div>
         <div class="criteria">
-          <input type="checkbox" name="worldCat" />
+          <input type="checkbox" name="worldCat" <?php if(isset($_GET['worldCat'])) echo "checked='checked'"; ?> />
           <label>Search OCLC</label>
         </div>
   </form>
+  <?php 
+  if (isset($_GET['safeCreative'])) {
+    include("SafeCreativeAPI.search.php");
+  }
+  if (isset($_GET['worldCat'])) {
+    include("WorldCatAPI.search.php");
+  }
+  ?>
+
 </body>
 </html>
